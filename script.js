8e2a05e39b10661d996ca99160f48eda791fd271
sivapucc <124483364+sivapucc@users.noreply.github.com>
2023-04-09 00:10:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function foo()
+{
+    var first=document.getElementById("firstname").value;
+    var mid=document.getElementById("middlename").value;
+    var last=document.getElementById("lastname").value;
+    console.log(first);
+    console.log(mid);
+    console.log(last);
+
+}
